9769e464fc17c8f3ae39531874fb1c141a09f1b5
roshinikotagiri <roshinikotagiri21@gmail.com>
2020-06-22 23:14:51 +0530

    Updates

---
+++ precache-manifest.ab86af78d04e1af7bdd46fa153d8fe35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd10d6287a381b8ee3656a630a0c1694",
+    "url": "/roshini-profile/index.html"
+  },
+  {
+    "revision": "ae09b2c0dc984d9e6d89",
+    "url": "/roshini-profile/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ad28dd465ca92ec9b26",
+    "url": "/roshini-profile/static/js/2.5fe9da17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roshini-profile/static/js/2.5fe9da17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae09b2c0dc984d9e6d89",
+    "url": "/roshini-profile/static/js/main.7166916d.chunk.js"
+  },
+  {
+    "revision": "669db752e6f1854fd169",
+    "url": "/roshini-profile/static/js/runtime-main.df80ca7d.js"
+  },
+  {
+    "revision": "350ff683ce0bca2cc0a3fc5029dffb89",
+    "url": "/roshini-profile/static/media/profile.350ff683.svg"
+  }
+]);
